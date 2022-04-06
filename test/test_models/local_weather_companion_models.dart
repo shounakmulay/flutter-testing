7eb36de19d5c5ad26a15f183a01d7a49cba31c56
Shounak Mulay <shounakmulay@gmail.com>
2022-04-06 07:54:13 +0530

    Clean test directory

--- test/test_models/local_weather_companion_models.dart
+++
@@ -1,7 +0,0 @@
-import 'package:drift/drift.dart';
-import 'package:flutter_testing/services/base/database/app_database.dart';
-
-const localWeatherCompanion = LocalWeatherCompanion(
-  cityWoeid: Value(1),
-  title: Value("title 1"),
-);
--- test/test_models/ui_city_models.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_testing/presentation/entity/weather/ui_city.dart';
-
-final uiCity = UICity(
-  cityId: 1,
-  title: "title",
-  locationType: "locationType",
-  location: "location",
-  isFavourite: false,
-);
